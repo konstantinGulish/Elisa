a25c9c0c61b7cc8aeae6abcadfeeafcfae0d60f3
konstantinGulish <konstantin_gulish@yahoo.com>
2018-06-04 14:52:05 -0400

    Initial commit

---
+++ src/Elisa.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Elisa {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String prompt = "";
+        System.out.print("Good day. What is your problem? " );
+        while (!prompt.equals("Q") || !prompt.equals("q")) {
+            System.out.print("Enter your response here or Q to quit: ");
+            prompt = in.nextLine();
+            if (prompt.equals("Q") || prompt.equals("q"))  break;
+                System.out.println(respond(prompt));
+        }
+
+    }
+
+    private static String respond(String prompt) {
+        String response = prompt;
+        Random rnd = new Random();
+        int responseType = rnd.nextInt(3);
+        if (responseType == 1) {
+            response = replacePronouns(response);
+
+            String [] qualifiers = {"Why do you say that", "You seem to think that", "So, you are concerned that"};
+            response =  qualifiers [rnd.nextInt(qualifiers.length)]+ " " + response + "?";
+        }
+        else {
+            String [] hedges = {"Please tell me more", "Many of my patients tell me the same thing", "It is getting late, maybe we had better quit"};
+            response = hedges [rnd.nextInt(hedges.length)];
+        }
+        return response;
+    }
+
+    private static String replacePronouns(String response) {
+        String [] responses = {"me", "my", "I", "am"};
+        String [] replacements = {"you", "your", "you", "are"};
+        for (int i = 0; i < responses.length; i++) {
+            response = response.replace(responses[i], replacements[i]);
+        }
+        return response;
+    }
+}
